492b83e69f478d879fd971b8c6e7273aad425a50
Alfie-mx <alfredo.rdz.joya@gmail.com>
2016-10-30 14:21:15 -0700

    Persisting State with Google Firebase

---
+++ src/base.js
@@ -0,0 +1,14 @@
+/**
+ * Created by alfiemx on 2016-10-30.
+ */
+
+import Rebase from 're-base'
+
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyAyqB7UTkVW9jp5xBfYHYVx6T4MRzovzyE",
+    authDomain: "catch-of-the-day-alfiemx.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-alfiemx.firebaseio.com"
+});
+
+export default base;
