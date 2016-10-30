210c936b39ac2a6f868cd0830139378e3819a99d
Alfie-mx <alfredo.rdz.joya@gmail.com>
2016-10-29 17:14:34 -0700

    Added React Router

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+/**
+ * Created by alfiemx on 2016-10-29.
+ */
+
+import React from 'react'
+
+
+export default class NotFound extends React.Component {
+    render() {
+        return (
+            <h2>Not Found!!!!</h2>
+        )
+    }
+}
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,14 +1,43 @@
+/**
+ * Created by alfiemx on 2016-10-29.
+ */
+
 import React from 'react'
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+    constructor(){
+        super();
+
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    goToStore(ev){
+        ev.preventDefault();
+        // first grab the text from the box
+        const storeId = this.storeInput.value;
+        // second transition from / to /store/:storeId
+        this.context.router.transitionTo(`/store/${storeId}`)
+    }
+
     render(){
         return (
-            <form className="store-selector">
-                <input type="text" placeholder="Store Name" required="required"/>
+            <form className="store-selector" onSubmit={this.goToStore}>
+                <input type="text"
+                       placeholder="Store Name"
+                       required="required"
+                       defaultValue={getFunName()}
+                       ref={(input) => {this.storeInput = input} }
+                />
+
                 <button type="submit">Visit Store -></button>
             </form>
         )
     }
 }
+
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
+};
 
 export default StorePicker
